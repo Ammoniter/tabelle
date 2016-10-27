180a1f41f7b852d7bdb9e213e16d9162da3cfa0e
Public <Public>
2016-10-27 08:51:52 +0200

    Test3

---
+++ formular.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Formular</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<section class="container">
+		<div class="row">
+			<h1>mein Formular</h1>
+			<div class="col-md-12">
+
+				<form action="senden.php" method="get">
+				<input type="text" name="vorname" class="form-control "/>
+			    <button type="submit" value="übermitteln" class="form-control btn btn-default" /> Absenden
+			    </button>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
